c7f37ce19951393fb6c519135dfbd4deff01d417
Hikaru Miyata <m.shirokumakingdom@gmail.com>
2023-02-09 15:15:16 -0800

    first commit to preserve data

---
+++ index.js
@@ -0,0 +1,117 @@
+//Select elements
+const productShop = document.querySelector("#shop");
+const Total = document.querySelector("#total");
+const reset = document.getElementById("reset");
+let cartAmount = document.getElementsByClassName("cartAmount")[0];//この行２行あるけど
+// number = insideCart.length;
+let elements = document.getElementById("cart-container");
+//Render products
+function renderProducts() {
+  fetch("./products.json").then(res => res.json())
+    .then(data => {
+      data.forEach((product) => {
+        productShop.innerHTML += `<div class="products">
+        <img width="200" src="${product.imageUrl}" alt="${product.name}">
+        <div class="details">
+            <h3>${product.name}</h3>
+            <p>${product.description}</p>
+            <div class="price-quantity">
+                <h2>${product.price}</h2>
+                <div class="buttons">
+                    <i class="bi bi-dash-square-fill"></i>
+                    <div class="quantity">0</div>
+                    <i class="bi bi-plus-square-fill"></i>
+                    <button onclick="addItem(${product.id})" class="add-cart">add to cart</button>
+                </div>
+            </div>
+        </div>
+      </div>`
+      });
+    })
+}
+renderProducts();
+let insideCart = [];
+let addCartButton = document.querySelectorAll(".add-cart");
+//Add item to cart
+function addItem(itemId){
+  fetch("./products.json").then(res =>res.json())
+    .then(data =>{
+      let item = data.find(item => item.id === itemId);
+      insideCart.push(item);
+       //change the number of cart
+   number = insideCart.length;
+  console.log(number);
+  let cartAmount = document.getElementsByClassName("cartAmount")[0];//getを使うときは宣言する必要がある
+  cartAmount.innerHTML = number;
+      renderCartItems(insideCart);
+
+      //increment the number of unit
+      let dataObj = JSON.parse(data);
+      dataobj.count += 1;
+
+    });
+}
+
+//increase the number of item unit
+function increseUnits (){
+  const fs = require('fs');
+
+fs.readFile('./data.json', 'utf-8', (err, data) => {
+  if (err) throw err;
+
+  let dataObj = JSON.parse(data);
+
+  dataObj.count += 1;
+
+  fs.writeFile('./data.json', JSON.stringify(dataObj), (err) => {
+    if (err) throw err;
+    console.log('Value incremented and file updated successfully!');
+  });
+});
+
+
+
+}
+//Render cart items
+function renderCartItems(arr){
+  
+  elements.innerHTML ="";
+  let totalAmount = arr.reduce((acc, item) => acc + item.price, 0);
+  arr.forEach(product =>{
+    elements.innerHTML +=
+      `
+        <div id="cartItem">
+          <img src=${product.imageUrl}>
+          <div class="shousai">
+            <h6 id="name">${product.name}</h6>
+            <p id="price">$${product.price}</p>
+          </div>
+          <i class="bi bi-trash3"></i>
+        </div>
+      `
+  });
+  elements.innerHTML += `<h6 id="total">Total: $${totalAmount}</h6>
+                         <div class="Okaikei">
+                           <button onclick="resetitems()" id="reset">reset</button>
+                           <button id="checkout">check out</button>
+                         </div>`;
+  
+                         
+}
+
+function resetitems(){
+  elements.innerHTML = "";
+  cartAmount.innerHTML = 0;
+  insideCart.length= 0;
+}
+
+
+
+//あとは個々に消すデリーとボタンと、重複しないように表示、カートの中身を隠す（できれば）
+
+
+//チャット
+
+
+
+
