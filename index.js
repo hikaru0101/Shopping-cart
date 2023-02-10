080a84374dd4635548593790d7154734c59e1858
Hikaru Miyata <m.shirokumakingdom@gmail.com>
2023-02-10 05:13:31 -0800

    third commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const reset = document.getElementById("reset");
 let cartAmount = document.getElementsByClassName("cartAmount")[0];//この行２行あるけど
 // number = insideCart.length;
 let elements = document.getElementById("cart-container");
+let numberOfUnit = 1;
 
 
 //Render products　商品一覧
@@ -18,7 +19,7 @@ function renderProducts() {
             <h3>${product.name}</h3>
             <p>${product.description}</p>
             <div class="price-quantity">
-                <h2>${product.price}</h2>
+                <h2>$${product.price}</h2>
                 <div class="buttons">
                     
                     <button onclick="addItem(${product.id})" class="add-cart">add to cart</button>
@@ -59,7 +60,7 @@ function addItem(itemId){
 
 
 
-//Render cart items　カートの中の商品の表示　オブジェクトごと配列に入るから画像とかのプロパティにアクセスできる
+//Render cart items　カートの中の商品の表示　オブジェクトごと配列に入るから画像とかのプロパティにアクセスできる 動画のaddToCartを参考にした
 function renderCartItems(arr){
   
   elements.innerHTML ="";
@@ -80,17 +81,50 @@ function renderCartItems(arr){
         </div>
       `
   });
-  elements.innerHTML += `<h6 id="total">Total: $${totalAmount}</h6>
-                        <h6 id="total-quantity">Item Amount:${insideCart.length}</h6>
+  elements.innerHTML += `
+                        <div class="bgc">
+                        <h6 id="total">  Total: $${totalAmount}</h6>
+                        <h6 id="total-quantity">  Item Amount:${insideCart.length}</h6>
                          <div class="Okaikei">
                            <button onclick="resetitems()" id="reset">reset</button>
                            <button id="checkout">check out</button>
+                         </div>
                          </div>`;
   
                         //  zeroState();                     
 }
 
 
+// 保存用
+// function renderCartItems(arr){
+  
+//   elements.innerHTML ="";
+//   let totalAmount = arr.reduce((acc, item) => acc + item.price, 0);
+
+
+
+//   arr.forEach(product =>{
+//     elements.innerHTML +=
+//       `
+//         <div id="cartItem">
+//           <img src=${product.imageUrl}>
+//           <div class="shousai">
+//             <h6 id="name">${product.name}</h6>
+//             <p id="price">$${product.price}</p>
+//           </div>
+//           <i onclick="deleteHTML(${product.id})"class="bi bi-trash3"></i>
+//         </div>
+//       `
+//   });
+//   elements.innerHTML += `<h6 id="total">Total: $${totalAmount}</h6>
+//                         <h6 id="total-quantity">Item Amount:${insideCart.length}</h6>
+//                          <div class="Okaikei">
+//                            <button onclick="resetitems()" id="reset">reset</button>
+//                            <button id="checkout">check out</button>
+//                          </div>`;
+  
+//                         //  zeroState();                     
+// }
 function resetitems(){
   elements.innerHTML = "";
   cartAmount.innerHTML = 0;
--- test/test.js
+++ test/test.js
@@ -1,4 +1,22 @@
- let array=  [
+
+let array=  [
+  {
+    "id": 1,
+    "name": "Apple iPhone 12 Pro Max",
+    "description": "The latest iPhone model with advanced camera features and powerful performance.",
+    "imageUrl": "https://1.bp.blogspot.com/-NOU4Vn7yW84/X9GX3MNKruI/AAAAAAABcqU/FIFxd04wg_Qda1RMLrd0ThpTaRxiNB4jgCNcBGAsYHQ/s180-c/bird_crested_duck_ahiru.png",
+    "price": 1149.00,
+    
+  },
+
+  {
+      "id": 1,
+      "name": "Apple iPhone 12 Pro Max",
+      "description": "The latest iPhone model with advanced camera features and powerful performance.",
+      "imageUrl": "https://1.bp.blogspot.com/-NOU4Vn7yW84/X9GX3MNKruI/AAAAAAABcqU/FIFxd04wg_Qda1RMLrd0ThpTaRxiNB4jgCNcBGAsYHQ/s180-c/bird_crested_duck_ahiru.png",
+      "price": 1149.00,
+      
+    },
     {
       "id": 1,
       "name": "Apple iPhone 12 Pro Max",
@@ -7,24 +25,15 @@
       "price": 1149.00,
       
     },
-
-    {
-        "id": 1,
-        "name": "Apple iPhone 12 Pro Max",
-        "description": "The latest iPhone model with advanced camera features and powerful performance.",
-        "imageUrl": "https://1.bp.blogspot.com/-NOU4Vn7yW84/X9GX3MNKruI/AAAAAAABcqU/FIFxd04wg_Qda1RMLrd0ThpTaRxiNB4jgCNcBGAsYHQ/s180-c/bird_crested_duck_ahiru.png",
-        "price": 1149.00,
-        
-      },
-      {
-        "id": 1,
-        "name": "Apple iPhone 12 Pro Max",
-        "description": "The latest iPhone model with advanced camera features and powerful performance.",
-        "imageUrl": "https://1.bp.blogspot.com/-NOU4Vn7yW84/X9GX3MNKruI/AAAAAAABcqU/FIFxd04wg_Qda1RMLrd0ThpTaRxiNB4jgCNcBGAsYHQ/s180-c/bird_crested_duck_ahiru.png",
-        "price": 1149.00,
-        
-      },
-    {
+  {
+    "id": 2,
+    "name": "Samsung Galaxy S21 Ultra",
+    "description": "A high-end smartphone with a large screen and top-of-the-line camera technology.",
+    "imageUrl": "https://1.bp.blogspot.com/-_0tLdAB6wVs/X7zMVCQ8k7I/AAAAAAABcZ4/BS-S_XwqcdkQJaX4wSVdkLi1dMVjQiInACNcBGAsYHQ/s180-c/bird_gachou.png",
+    "price": 1149.00,
+    
+  },
+  {
       "id": 2,
       "name": "Samsung Galaxy S21 Ultra",
       "description": "A high-end smartphone with a large screen and top-of-the-line camera technology.",
@@ -32,23 +41,107 @@
       "price": 1149.00,
       
     },
-    {
-        "id": 2,
-        "name": "Samsung Galaxy S21 Ultra",
-        "description": "A high-end smartphone with a large screen and top-of-the-line camera technology.",
-        "imageUrl": "https://1.bp.blogspot.com/-_0tLdAB6wVs/X7zMVCQ8k7I/AAAAAAABcZ4/BS-S_XwqcdkQJaX4wSVdkLi1dMVjQiInACNcBGAsYHQ/s180-c/bird_gachou.png",
-        "price": 1149.00,
-        
-      },
-    {
-      "id": 3,
-      "name": "Google Pixel 6 Pro",
-      "description": "A smartphone with a sleek design and advanced camera features powered by Google.",
-      "imageUrl": "https://1.bp.blogspot.com/-bxlD0epIwJY/X7zMVGrK_EI/AAAAAAABcZw/Xgmc3-cnfOsuGQqDw7GIyNjOPXkmXRKtwCNcBGAsYHQ/s180-c/bird_shiro_sagi.png",
-      "price": 799.00,
-      
-    }
-  ]
+  {
+    "id": 3,
+    "name": "Google Pixel 6 Pro",
+    "description": "A smartphone with a sleek design and advanced camera features powered by Google.",
+    "imageUrl": "https://1.bp.blogspot.com/-bxlD0epIwJY/X7zMVGrK_EI/AAAAAAABcZw/Xgmc3-cnfOsuGQqDw7GIyNjOPXkmXRKtwCNcBGAsYHQ/s180-c/bird_shiro_sagi.png",
+    "price": 799.00,
+    
+  }
+]
+
+function set (array){
+
+}
+
+
+// function renderCartItems(arr){
+//   let uniqueArray = [...new Set(arr)];
+//   elements.innerHTML ="";
+//   let totalAmount = uniqueArray.reduce((acc, item) => acc + item.price, 0);
+
+//   uniqueArray.forEach(product =>{
+//     elements.innerHTML +=
+//       `
+//         <div id="cartItem">
+//           <img src=${product.imageUrl}>
+//           <div class="shousai">
+//             <h6 id="name">${product.name}</h6>
+//             <h6 id="eachAmount">計</h6>
+//             <p id="price">$${product.price}</p>
+//           </div>
+//           <i onclick="deleteHTML(${product.id})"class="bi bi-trash3"></i>
+//         </div>
+//       `
+//   });
+//   elements.innerHTML += `<h6 id="total">Total: $${totalAmount}</h6>
+//                         <h6 id="total-quantity">Item Amount:${uniqueArray.length}</h6>
+//                          <div class="Okaikei">
+//                            <button onclick="resetitems()" id="reset">reset</button>
+//                            <button id="checkout">check out</button>
+//                          </div>`;
+// }
+// renderCartItems(array);
+
+
+  // function countDuplicates(array) {
+  //   let counts = {};
+  //   for (let i = 0; i < array.length; i++) {
+  //     let id = array[i].id;
+  //     if (counts[id]) {
+  //       counts[id]++;
+  //     } else {
+  //       counts[id] = 1;
+  //     }
+  //   }
+  //   return counts;
+  // }
+  
+  // // console.log(countDuplicates(array));
+
+  // function showRenderOnlyOneTime(array){
+   
+
+
+
+
+  //   //商品カウント
+    
+  // }
+
+  
+
+  // function renderCartItems(arr){
+  
+  //   elements.innerHTML ="";
+  //   let totalAmount = arr.reduce((acc, item) => acc + item.price, 0);
+  //   arr.forEach(product =>{
+  //     elements.innerHTML +=
+  //       `
+  //         <div id="cartItem">
+  //           <img src=${product.imageUrl}>
+  //           <div class="shousai">
+  //             <h6 id="name">${product.name}</h6>
+  //             <h6 id="eachAmount">計</h6>
+  //             <p id="price">$${product.price}</p>
+  //           </div>
+  //           <i onclick="deleteHTML()"class="bi bi-trash3"></i>
+  //         </div>
+  //       `
+  //   });
+  //   elements.innerHTML += `<h6 id="total">Total: $${totalAmount}</h6>
+  //                          <div class="Okaikei">
+  //                            <button onclick="resetitems()" id="reset">reset</button>
+  //                            <button id="checkout">check out</button>
+  //                          </div>`;
+    
+                           
+  // }
+
+
+
+
 //指定したいidを消す これは働く
 //   function removeElementsWithId(array, id) {
 //     return array.filter(function(item) {
@@ -71,30 +164,30 @@
 
 
 //jsonの中のcountの数を変えてくれる（笑）
-// function countDuplicates(array) {
-//     let countMap = new Map();
+function countDuplicates(array) {
+    let countMap = new Map();
   
-//     array.forEach(function(item) {
-//       let key = JSON.stringify(item);
-//       if (countMap.has(key)) {
-//         countMap.set(key, countMap.get(key) + 1);
-//       } else {
-//         countMap.set(key, 1);
-//       }
-//     });
+    array.forEach(function(item) {
+      let key = JSON.stringify(item);
+      if (countMap.has(key)) {
+        countMap.set(key, countMap.get(key) + 1);
+      } else {
+        countMap.set(key, 1);
+      }
+    });
   
-//     let duplicates = [];
-//     countMap.forEach(function(value, key) {
-//       if (value > 1) {
-//         duplicates.push({
-//           item: JSON.parse(key),
-//           count: value
-//         });
-//       }
-//     });
+    let duplicates = [];
+    countMap.forEach(function(value, key) {
+      if (value > 1) {
+        duplicates.push({
+          item: JSON.parse(key),
+          count: value
+        });
+      }
+    });
   
-//     return duplicates;
-//   }
+    return duplicates;
+  }
 
 
 //   let result = countDuplicates(array);
@@ -110,18 +203,5 @@
 //   console.log(count);
 // }
 
-function countDuplicates(array) {
-  let counts = {};
-  for (let i = 0; i < array.length; i++) {
-    let id = array[i].id;
-    if (counts[id]) {
-      counts[id]++;
-    } else {
-      counts[id] = 1;
-    }
-  }
-  return counts;
-}
 
-console.log(countDuplicates(array));
  
